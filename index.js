56c48c7049d43a26bc9003360a3db50a811e7498
Kartik Sehgal <Kartiksehgal3@gmail.com>
2020-12-24 22:51:24 +0530

    1

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.set("port", process.env.PORT || 5000);
 
 const corsOption = {
-  origin: "https://url-shortener-kartik.netlify.app/",
+  origin: "https://url-shortener-kartik.netlify.app",
   credentials: true,
 };
 
